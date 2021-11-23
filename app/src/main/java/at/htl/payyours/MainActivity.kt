70eac725b5f62e18e0e940ff674a3b3423ffd20e
schollsebastian <scholl.sebastian@gmx.at>
2021-11-23 14:56:28 +0100

    Implement MainActivity.onCreateOptionsMenu

--- app/src/main/java/at/htl/payyours/MainActivity.kt
+++ app/src/main/java/at/htl/payyours/MainActivity.kt
@@ -2,10 +2,34 @@ package at.htl.payyours
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_item_quit -> {
+                finish()
+                true
+            }
+
+            R.id.menu_item_setting -> {
+                Log.d("MainActivity", "Settings menu called")
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
